311fc703f745335e7051bfaf8037e60f833dde46
bot <anna.bot2021@campus-eni.fr>
2021-06-04 16:44:19 +0200

    mise en place du projet

---
+++ src/main/java/fr/eni/GestionPotager/GestionPotagerApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.GestionPotager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPotagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPotagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/GestionPotager/GestionPotagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.GestionPotager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPotagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

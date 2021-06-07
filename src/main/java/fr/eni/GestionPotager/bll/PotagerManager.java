824f87477f495d7d4b527a44e26f01e7f77b6f6a
Clement BERTRAN <clement.bertran2021@campus-eni.fr>
2021-06-07 14:36:51 +0200

    potagerManager

---
+++ src/main/java/fr/eni/GestionPotager/bll/PotagerManager.java
@@ -0,0 +1,59 @@
+package fr.eni.GestionPotager.bll;
+
+import java.util.List;
+
+import fr.eni.GestionPotager.bo.Potager;
+
+/**
+ * CRUD de Potager
+ * 
+ * @author cbertran2021
+ *
+ */
+public interface PotagerManager {
+
+	/**
+	 * Ajoute un potager
+	 * 
+	 * @param potager
+	 * @throws BllException 
+	 */
+	public void addPotager(Potager potager);
+
+	/**
+	 * Supprime un potager
+	 * 
+	 * @param potager
+	 */
+	public void removePotager(Potager potager);
+
+	/**
+	 * Met a jour un potager
+	 * 
+	 * @param potager
+	 */
+	public void updatePotager(Potager potager);
+
+	/**
+	 * Recupere tout les potager
+	 * 
+	 * @return liste de potager
+	 */
+	public List<Potager> getAllPotager();
+
+	/**
+	 * Recupere un potager par son identifiant
+	 * 
+	 * @param id
+	 * @return potager
+	 */
+	public Potager getPotagerById(Integer id);
+
+	/**
+	 * Supprime un potager par son identifiant
+	 * 
+	 * @param id
+	 */
+	public void removePotagerById(Integer id);
+
+}

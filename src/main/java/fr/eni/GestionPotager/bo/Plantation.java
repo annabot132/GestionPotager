758398e7d36158445d3f68ba3861a7e3b627337c
bot <anna.bot2021@campus-eni.fr>
2021-06-10 12:36:01 +0200

    ajout css

--- src/main/java/fr/eni/GestionPotager/bo/Plantation.java
+++ src/main/java/fr/eni/GestionPotager/bo/Plantation.java
@@ -29,7 +29,7 @@ public class Plantation {
 	@ManyToOne
 	private Carre carre;
 	
-	@ManyToOne()
+	@ManyToOne
 	private Plante plante;
 
 	public Plantation(LocalDate miseEnPlace, LocalDate recolte, Integer quantite, Carre carre, Plante plante) {

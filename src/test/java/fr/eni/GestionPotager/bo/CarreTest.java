0a71d39133c3e3ad5f3953de2dc57ca5b4097c09
Clement BERTRAN <clement.bertran2021@campus-eni.fr>
2021-06-14 18:10:52 +0200

    fix properties

--- src/test/java/fr/eni/GestionPotager/bo/CarreTest.java
+++ src/test/java/fr/eni/GestionPotager/bo/CarreTest.java
@@ -65,7 +65,7 @@ class CarreTest {
 	void testSetSurface() {
 		Potager potager = new Potager("STR", "STR", 25, "STR");
 		Carre carre1 = new Carre(2, "sableux", Exposition.MI_OMBRE, potager);
-		carre1.setSurface(12);
+		carre1.setSurface((double) 12);
 		assertEquals(12, carre1.getSurface());
 	}
 
